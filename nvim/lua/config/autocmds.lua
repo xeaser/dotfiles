05d4a53dc70c4af2281c936dc20be5592617e1f9
Parag Sharma <sharmarag93@gmail.com>
2026-02-22 03:51:52 +0530

    Add exrc autocommand to source .nvim.lua on project switch
    Neovim exrc only fires at startup, not on directory change. This adds a DirChanged + SessionLoadPost autocommand that sources .nvim.lua via vim.secure.read() when switching projects with neovim-project or manual :cd.

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd({ "DirChanged", "SessionLoadPost" }, {
+  group = vim.api.nvim_create_augroup("source_exrc", { clear = true }),
+  desc = "Source .nvim.lua on project switch or directory change",
+  callback = function(args)
+    local contents = vim.secure.read(string.format("%s/.nvim.lua", args.file))
+    if contents then
+      assert(loadstring(contents))()
+    end
+  end,
+})
